ff756c2129e331242fd33ed3ad0eb52ca1716b0a
Namit Malasi <namitmalasi@gmail.com>
2023-11-04 13:14:34 +0530

    setting up react router dom

--- client/src/App.js
+++ client/src/App.js
@@ -1,13 +1,47 @@
 import Footer from "./components/Footer";
 import Header from "./components/Header";
+import Cart from "./pages/Cart";
 import Home from "./pages/Home";
+import {
+  createBrowserRouter,
+  Outlet,
+  RouterProvider,
+  ScrollRestoration,
+} from "react-router-dom";
+
+const Layout = () => {
+  return (
+    <div>
+      <Header />
+      <ScrollRestoration />
+      <Outlet />
+      <Footer />
+    </div>
+  );
+};
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <Layout />,
+    children: [
+      {
+        path: "/",
+        element: <Home />,
+      },
+
+      {
+        path: "/cart",
+        element: <Cart />,
+      },
+    ],
+  },
+]);
 
 function App() {
   return (
     <div className="font-bodyFont">
-      <Header />
-      <Home />
-      <Footer />
+      <RouterProvider router={router} />
     </div>
   );
 }
