4b27f1f230bc8ef374aebe9640e98fd2ba798c21
Namit Malasi <namitmalasi@gmail.com>
2023-11-06 20:15:37 +0530

    added some more reducers

--- client/src/components/Header.js
+++ client/src/components/Header.js
@@ -32,12 +32,14 @@ const Header = () => {
             </li>
           </ul>
 
-          <div className="relative">
-            <img className="w-6" src={cartImg} alt="cartImg" />
-            <span className="absolute w-6 top-2 left-0 text-sm flex items-center justify-center font-semibold font-titleFont">
-              {productData.length}
-            </span>
-          </div>
+          <Link to="/cart">
+            <div className="relative">
+              <img className="w-6" src={cartImg} alt="cartImg" />
+              <span className="absolute w-6 top-2 left-0 text-sm flex items-center justify-center font-semibold font-titleFont">
+                {productData.length}
+              </span>
+            </div>
+          </Link>
           <img
             className="w-8 h-8 rounded-full"
             src="https://images.pexels.com/photos/264547/pexels-photo-264547.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1"
--- client/src/redux/bazaarSlice.js
+++ client/src/redux/bazaarSlice.js
@@ -17,11 +17,43 @@ export const bazaarSlice = createSlice({
       if (item) {
         item.quantity += action.payload.quantity;
       } else {
-        state.productData.push(action.payload)
+        state.productData.push(action.payload);
       }
+    },
+    increamentQuantity: (state, action) => {
+      const item = state.productData.find(
+        (item) => item._id === action.payload._id
+      );
+      if (item) {
+        item.quantity++;
+      }
+    },
+    decrementQuantity: (state, action) => {
+      const item = state.productData.find(
+        (item) => item._id === action.payload._id
+      );
+      if (item.quantity === 1) {
+        item.quantity = 1;
+      } else {
+        item.quantity--;
+      }
+    },
+    deleteItem: (state, action) => {
+      state.productData = state.productData.filter(
+        (item) => item._id !== action.payload
+      );
+    },
+    resetCart: (state) => {
+      state.productData = [];
     },
   },
 });
 
-export const { addToCart } = bazaarSlice.actions;
+export const {
+  addToCart,
+  deleteItem,
+  resetCart,
+  increamentQuantity,
+  decrementQuantity,
+} = bazaarSlice.actions;
 export default bazaarSlice.reducer;
