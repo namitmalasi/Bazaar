a6e4852d12d8ece7928a0dd94000dde21c1f8caa
Namit Malasi <namitmalasi@gmail.com>
2023-11-07 12:45:44 +0530

    added redux persist for persisting the state

--- client/src/redux/store.js
+++ client/src/redux/store.js
@@ -1,8 +1,32 @@
 import { configureStore } from "@reduxjs/toolkit";
+import {
+  persistStore,
+  persistReducer,
+  FLUSH,
+  REHYDRATE,
+  PAUSE,
+  PERSIST,
+  PURGE,
+  REGISTER,
+} from "redux-persist";
+import storage from "redux-persist/lib/storage";
 import bazaarReducer from "./bazaarSlice";
 
+const persistConfig = {
+  key: "root",
+  version: 1,
+  storage,
+};
+const persistedReducer = persistReducer(persistConfig, bazaarReducer);
+
 export const store = configureStore({
-  reducer: {
-    bazaar: bazaarReducer,
-  },
+  reducer: { bazaar: persistedReducer },
+  middleware: (getDefaultMiddleware) =>
+    getDefaultMiddleware({
+      serializableCheck: {
+        ignoredActions: [FLUSH, REHYDRATE, PAUSE, PERSIST, PURGE, REGISTER],
+      },
+    }),
 });
+
+export let persistor = persistStore(store);
